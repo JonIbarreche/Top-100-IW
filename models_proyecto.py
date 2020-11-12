66141a880bdc45296f22460aa5d1a2b42f68e7a9
David <33113652+DavidLopezLoureiro@users.noreply.github.com>
2020-11-12 10:25:49 +0100

    Add files via upload

---
+++ models_proyecto.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+class Cancion(models.Model):
+ titulo = models.CharField(max_length=30)
+ interprete = models.ManyToManyField(Interprete)
+ fecha = models.DateField()
+ estilo = models.ForeignKey(Estilo, on_delete=models.CASCADE)
+ 
+ def __str__(self):
+  return self.titulo
+  
+class Estilo(models.Model):
+ nombre = models.CharField(max_length=30)
+ 
+ def __str__(self):
+  return self.nombre
+  
+class Interprete(models.Model):
+ nombre = models.CharField(max_length=30)
+ DNI = models.CharField(max_length=9)
+ fecha = models.DateField()
+ descripcion = models.TextField(max_length=1000)
+ pass
+ 
+ def __str__(self):
+  return self.nombre

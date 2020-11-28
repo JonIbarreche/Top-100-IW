158aee35de516c9a46cfc7b7f24fe727bc37b3bd
David <33113652+DavidLopezLoureiro@users.noreply.github.com>
2020-11-28 23:33:15 +0100

    Update views.py

--- views.py
+++ views.py
@@ -6,10 +6,9 @@ from myapp.models import Cancion
 
 
 def index(request):
- #posts_list = Post.objects.all()
  elementos_list = Estilo.objects.all()
  canciones = Cancion.objects.all().order_by('popularidad')[:100]
- context = {'elementos' : elementos_list}
+ context = {'elementos' : elementos_list, "canciones" : canciones}
  return render(request, 'myapp/index.html', context)
  
 def cancion_detalle(request, id_cancion):
@@ -21,18 +20,15 @@ def cancion_detalle(request, id_cancion):
  return render(request, 'myapp/cancion.html', context)
  
 def estilo_detalle(request, id_estilo):
- 
  estilo = Estilo.objects.get(id = id_estilo)
  cancion_list = Cancion.objects.all()
  context = {"estilo" : estilo, "canciones" : cancion_list}
  
  return render(request, 'myapp/estilo.html', context)
  
-
 def interprete_detalle(request, id_interprete):
  interprete = Interprete.objects.get(id = id_interprete)
- cancion_list = Cancion.objects.all()
  interpretes = interprete.cancion_set.all()
- context = {"interprete" : interprete, "canciones" : cancion_list, "interpretes" : interpretes} 
+ context = {"interprete" : interprete, "interpretes" : interpretes} 
  
  return render(request, 'myapp/interprete.html', context)
